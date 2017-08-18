03475f4213bff1c9ea42c12014ce66637cd514bc
Loyd Charles <loydcharles3@gmail.com>
2017-08-18 08:30:09 -0400

    recent changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,7 +15,7 @@ connection.connect(function(err) {
   console.log("Welcome to BAmazon");
   if (err) throw err;
 
-  connection.query("SELECT item_id, product_name, price FROM products", function(err, res) {
+  connection.query("SELECT item_id, product_name, price, stock_quantity FROM products", function(err, res) {
     if (err) throw err;
  //   console.table(res);
     start(res);
@@ -29,7 +29,7 @@ function start(res) {
       name: "option",
       type: "list",
       message: "Would you like to VIEW or Buy an Item?",
-      choices: ["VIEW ITEMS", "BUY", "EXIT"]
+      choices: ["VIEW ITEMS", "BUY AN ITEM", "EXIT"]
     })
     .then(function(answer) {
       // based on their answer, either call the bid or the post functions
@@ -37,12 +37,36 @@ function start(res) {
         console.table(res);
         start();
       }
-      else if (answer.option.toUpperCase() === "BUY") {
+      else if (answer.option.toUpperCase() === "BUY AN ITEM") {
         console.log("Here is the items for sale");
         console.table(res);
+        buyItem(res);
       }
       else if (answer.option.toUpperCase() === "EXIT") {
         process.exit(1);
       }
+    });
+}
+
+function buyItem(res){
+   inquirer
+    .prompt([
+    {
+      name: "item",   
+      type: "input",   
+      message: "Please enter item_id for purchase?"      
+    },
+    {
+      name: "quantity",
+      type: "input",
+      message: "How many items would you like to buy?"
+    }]).then(function(order){      
+        console.log(order.quantity);
+        console.log(res[order.item-1].stock_quantity); 
+        if(order.quantity > res[order.item-1].stock_quantity) {
+      //the order we have start from 1 where the object from DB starts from 0
+         console.log("We don't have that many in stock, Please try again");
+         start(res);
+       }
     });
 }
